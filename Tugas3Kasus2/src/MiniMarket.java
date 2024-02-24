013f884c8e1a21f3cba4f93e8db2b75d6efaaef1
NobbyDK <159523433+NobbyDK@users.noreply.github.com>
2024-02-24 22:47:16 +0700

    MiniMarket.java

--- Tugas3Kasus2/src/MiniMarket.java
+++ Tugas3Kasus2/src/MiniMarket.java
@@ -12,10 +12,10 @@ import java.util.Scanner;
 public class MiniMarket {
     public static void main(String[] args) {
         
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);   
 
         
-        Penjualan Produk = new Penjualan(10);          //Membuat objek Penjualan dengan kapasitas maksimum 10 produk
+        Penjualan Produk = new Penjualan(10);     //Membuat objek Penjualan dengan kapasitas maksimum 10 produk
 
         //List produk
         Produk.tambahProduk(new Produk("Lays", 28_000, 100));
@@ -33,9 +33,9 @@ public class MiniMarket {
         List<PesananPembeli> daftarPesanan = new ArrayList<>();
 
         char pesanLagi;
-        do {                // Loop untuk memesan produk secara berulang
+        do {                //Loop untuk memesan produk secara berulang
             System.out.println("\n");
-            Produk.tampilProdukList(); // Menampilkan Produk yang tersedia di MiniMarket
+            Produk.tampilProdukList(); //Menampilkan Produk yang tersedia di MiniMarket
             System.out.println("\n");
             System.out.println("\tPesanan yang akan Dipesan");
             System.out.println("========================================");
@@ -43,11 +43,11 @@ public class MiniMarket {
             String namaMenu = input.nextLine();
             System.out.print("Masukkan jumlah yang ingin dipesan: ");
             int jumlahPesan = input.nextInt();
-            input.nextLine(); // Membersihkan newline di buffer
+            input.nextLine(); //Membersihkan newline di buffer
 
-            Produk.pesanProduk(namaMenu, jumlahPesan);    // Memesan Produk berdasarkan nama dan jumlah yang dimasukkan pengguna
+            Produk.pesanProduk(namaMenu, jumlahPesan);    //Memesan Produk berdasarkan nama dan jumlah yang dimasukkan pengguna
 
-            daftarPesanan.add(new PesananPembeli(namaMenu, jumlahPesan));    // Menambahkan pesanan pembeli ke dalam daftarPesanan
+            daftarPesanan.add(new PesananPembeli(namaMenu, jumlahPesan));    //Menambahkan pesanan pembeli ke dalam daftarPesanan
 
             System.out.print("Ingin memesan lagi? (y/n): ");
             pesanLagi = input.nextLine().charAt(0);
@@ -58,10 +58,9 @@ public class MiniMarket {
         System.out.println("\t Rincian Pesanan");
         System.out.println("========================================"); 
         Produk.tampilkanRincianPesanan( daftarPesanan);
-        Produk.totalpembayaran(daftarPesanan );          // Menampilkan total pembayaran untuk semua pesanan
+        Produk.totalpembayaran(daftarPesanan );          //Menampilkan total pembayaran untuk semua pesanan
         input.close();
     }
 }
-
 
 
